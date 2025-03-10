90a3fe9e68ddef640202cd6136bd549697c04f0b
TerABriM <saurondgabalov776@gmail.com>
2025-03-10 21:15:56 +0300

    111

--- Project 0.6/login.php
+++ Project 0.6/login.php
@@ -3,6 +3,21 @@ require_once('db.php');
 $login = $_POST['login'];
 $pass = $_POST['pass'];
 
+$mysqli = new mysqli("user", "pass", "db.php");
+
+// Проверка соединения
+if ($mysqli->connect_error) {
+    die("Ошибка подключения: " . $mysqli->connect_error);
+}
+
+// Подготовка SQL-запроса для поиска пользователя по логину
+$stmt = $mysqli->prepare("SELECT id, password FROM users WHERE login = ?");
+$stmt->bind_param("s", $login); // "s" означает string
+
+// Выполнение запроса
+$stmt->execute();
+$result = $stmt->get_result();
+
 if (empty($login) || empty($pass))
 {
     echo "Заполните все поля";
